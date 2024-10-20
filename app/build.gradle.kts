3ca7d05030d236e0332d31ce4ca8202794a70e66
Hello.rtr <hello.worldrtr@mail.ru>
2024-10-20 23:01:00 +0500

    nitial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.bignerdranch.android.pr23_1"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.bignerdranch.android.pr23_1"
+        minSdk = 25
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation ("com.google.android.support:wearable:2.8.1")
+    implementation ("com.google.android.material:material:1.3.0")
+    implementation("androidx.wear:wear:1.3.0") // Основная библиотека для Wear
+    implementation("androidx.wear.compose:compose-material:1.1.2") // Material Design для Wear
+    implementation("androidx.activity:activity-compose:1.7.2") // Compose Activity
+    implementation("androidx.compose.ui:ui:1.4.0") // Основная библиотека Compose
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.0") // Инструменты для предварительного просмотра
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.0")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.8.0")
+}
